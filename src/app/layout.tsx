b70510fe332c19e995abe5155d0b03487a08301c
Harris <shaohaizhou36@gmail.com>
2025-10-15 21:13:24 +0800

    fix google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,17 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             });
           `}
         </Script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-17643358883">
+        </Script>
+        <Script>  
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-17643358883');
+          `}
+        </Script>
       </head>
       <body>{children}</body>
     </html>
